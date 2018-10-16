06ca27ec08040f63696c806e970fa941a4981ada
Viraj Desai <34795652+smaaashv2@users.noreply.github.com>
2018-10-16 20:09:47 +0530

    Update converterMain.py

--- converterMain.py
+++ converterMain.py
@@ -1,5 +1,8 @@
-#Main program for the application
+#function: main controller program for the application
+#inputs: integer n 
+#output: depending on selected function
 #author: Viraj Desai
+#last edited: 9/10/2018
 
 import getData as gd
 import register as reg
@@ -16,7 +19,7 @@ login_status=False
 
 if __name__ == '__main__':
 	print("Welcome to currency converter")
-	while True:
+	while True:								#The main menu which provides all the functionalities
 		print("------------------------------------------------------------------")
 		print("Press 1 to register")
 		print("Press 2 to Login")
@@ -29,43 +32,46 @@ if __name__ == '__main__':
 		print("Press 9 to logout and exit")
 		n=int(input())
 		print("------------------------------------------------------------------")
-		if n==1:
+		if n==1:							#registration of new user 
 			reg.register_user()
-		elif n==2:
-			login_status=lg.login()
-		elif n==3:
-			if login_status:
+		elif n==2:							#login for user
+			if login_status:					#checks whether another or same user is already logged in
+				print("User already logged in")
+			else:
+				login_status=lg.login()
+		elif n==3:							#for comparing costs			
+			if login_status:					#works only if user is logged in
 				cc.compare()
 			else:
 				print("Please login")
-		elif n==4:
-			if not login_status:
+		elif n==4:							#pay
+			if not login_status:					#works only if user is logged in
 				print("Please login")
 			elif pp.pay():
 				print("Payment successfull")
 			else:
 				print("Payment unsuccessfull! Enter valid usernames or amount!")
-		elif n==5:
-			if login_status:
+		elif n==5:							#receive
+			if login_status:					#works only if user is logged in
 				rec.receive()
 			else:
 				print("Please login")
-		elif n==6:
-			if login_status:
-				ct.convert()
+		elif n==6:							#Converting amount
+			if login_status:					#works only if user is logged in
+				ct.convert()	
 			else:
 				print("Please login")
-		elif n==7:
-			if login_status:
+		elif n==7:							#Display conversion rates
+			if login_status:					#works only if user is logged in
 				dr.display()
 			else:
 				print("Please login")
-		elif n==8:
-			if login_status:
-				hf.help()
+		elif n==8:							#help and FAQs
+			if login_status:					#works only if user is logged in
+				hf.help()		
 			else:
 				print("Please login")
-		elif n==9:
+		elif n==9:							#log out and exit the program
 			print("Successfully logged out! Thank you!")
 			break
 		else:
